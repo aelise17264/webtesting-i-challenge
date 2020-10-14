8f8ec51956e85eef57bd0d73747bd3bdd9c03560
unknown <aelise17264@gmail.com>
2020-10-14 12:36:37 -0600

    first test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
-const enhancer = require('./enhancer.js');
+const { success,
+    fail,
+    repair,
+    get,} = require('./enhancer.js');
 // test away!
+
+describe('repair()', () => {
+    it('should return the durability of an item to 100',() => {
+        expect(repair('durability')).toEqia;({durability: 100})
+    })
+})
