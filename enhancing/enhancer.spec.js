90510db879ac979f4aaccf6ff64fde352a4ff576
unknown <aelise17264@gmail.com>
2020-10-14 13:43:01 -0600

    updated first test, 2nd test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,6 +6,18 @@ const { success,
 
 describe('repair()', () => {
     it('should return the durability of an item to 100',() => {
-        expect(repair('durability')).toEqia;({durability: 100})
+       const repairedItem = repair({item: 'item', durability: 50})
+        expect(repairedItem.durability).toBe(100)
     })
 })
+
+describe('success()', () => {
+it('should increase the enhancement by 1', () => {
+    const enhancedItem = success({ item: 'item', enhancement: 10})
+    expect(enhancedItem.enhancement).toBe(11)
+})
+})
+
+// describe('fail()', () => {
+    
+// })
